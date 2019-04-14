608720c9fc590e1dfd8663f6dbc863dd4023a317
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-14 03:01:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,6 +47,12 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  for(var i = 0; i < cart; i++) {
+    if(cart[i].itemName === item) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
   return "That item is not in your cart."
 }
 
