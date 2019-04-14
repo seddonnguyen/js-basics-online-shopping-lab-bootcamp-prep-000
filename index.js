dc4a85144728e6a15dd504591e8d34b6ab990364
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-14 02:41:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ function addToCart(item) {
 function viewCart() {
   // write your code here
   var temp = "";
+  
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+  }
+  
   for(var i = 0; i < cart.length; i++) {
     if(i === 0) {
       temp = `In your cart, you have ${cart[i].itemName} at ${cart[i].itemPrice}`;
