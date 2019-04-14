e093afdb07523d0d034cdb44544273c6992e8642
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-14 02:55:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber === undefined) {
-    console.log("Sorry, we don't have a credit card on file for you.")
+    return "Sorry, we don't have a credit card on file for you.";
   } else {
     var num = total();
     cart = [];
